bb8cc84fa6dd347f9e9af0af5406ad9d59890998
Sanket Patil <sanketpatil574918@gmail.com>
2025-07-08 09:42:19 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React {useEffect} from 'react'
 import About from "./About";
 import Contact from "./Contact";
 import Projects from "./Projects";
 import Skills from "./Skills";
 
 export default function Home() {
+  useEffect(()=>{
+    const utterance = new SpeechSynthesisUtterance("Welcome to Sanket Portfolio");
+   window.speechSynthesis.speak(utterance);
+  },[]);
   return (
     <>
       <About/>
